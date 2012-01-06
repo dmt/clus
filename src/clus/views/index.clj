f33d07fbd36d794f649413e94ee4eacc12a69a66
Daniel Temme <dtemme@gmail.com>
2012-01-06 18:56:54 +0100

    add store call to index page

--- src/clus/views/index.clj
+++ src/clus/views/index.clj
@@ -1,11 +1,11 @@
 (ns clus.views.index
-  (:require [clus.views.common :as common])
+  (:require [clus.views.common :as common]
+            
+            )
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
-        [hiccup.form-helpers]))
-
-(defonce ids->values (atom {}))
-(defonce counter (atom 0))
+        [hiccup.form-helpers]
+        [clus.service.store-url :only [store]]))
 
 (defpage "/" {:as params}
          (common/layout
@@ -13,8 +13,7 @@
           (form-to [:post "/create"]
                    (common/form-fields params))))
 
-(defpage [:post "/create" ] {:as params}
-  (swap! ids->values assoc (swap! counter + 1) (:targetUrl params))
-  (common/layout [:p (str "created " @counter " for " (:targetUrl params))])
-  )
+(defpage [:post "/create" ] {:as params} 
+  (let [targetUrl (:targetUrl params) id (store targetUrl)]
+   (common/layout [:p (str "created " id " for " targetUrl)])))
 
